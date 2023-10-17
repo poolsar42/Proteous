63fbc859e6e5dd6e96441006737763ea7bf46494
emil <33105890+poolsar42@users.noreply.github.com>
2023-10-17 14:25:17 +0200

    3d prot

--- src/App.tsx
+++ src/App.tsx
@@ -2,18 +2,58 @@ import logo from "./images/Proteous.png";
 import Table from "./components/Table";
 import { useEffect, useState } from "react";
 import axios from "axios";
+import Dropdown from "react-dropdown";
+import "react-dropdown/style.css";
+import toast, { Toaster } from "react-hot-toast";
+
+import img0 from "./protein_images/0.gif";
+import img1 from "./protein_images/1.gif";
+import img2 from "./protein_images/2.gif";
+import img3 from "./protein_images/3.gif";
+import img4 from "./protein_images/4.gif";
+import img5 from "./protein_images/5.gif";
+import img6 from "./protein_images/6.gif";
+import img7 from "./protein_images/7.gif";
+import img8 from "./protein_images/8.gif";
+import img9 from "./protein_images/9.gif";
+import img10 from "./protein_images/10.gif";
+
+const options = [
+  "Increase protein Tm",
+  "Optimize protein expression",
+  "Increase custom protein activity",
+];
+const defaultOption = options[0];
+
+const proteinImages = {
+  0: img0,
+  1: img1,
+  2: img2,
+  3: img3,
+  4: img4,
+  5: img5,
+  6: img6,
+  7: img7,
+  8: img8,
+  9: img9,
+  10: img10,
+};
+
+function getRandomInt(max) {
+  return Math.floor(Math.random() * max);
+}
 
 function App() {
   const [entries, setEntries] = useState([]);
-  const [id, setId] = useState(0);
-  const [prediction, setPrediction] = useState([]);
+  const [prediction, setPrediction] = useState({ sequences: [], tms: [] });
   const [substring, setSubstring] = useState("");
-  const [tm, setTm] = useState("");
+
+  const notify = () => toast("Copied");
 
   useEffect(() => {
     axios
-      .get("https://proteous-606b94fbd95c.herokuapp.com/all-entries")
-      // .get("http://localhost:8000/all-entries")
+      // .get("https://proteous-606b94fbd95c.herokuapp.com/all-entries")
+      .get("http://localhost:8000/all-entries")
       .then((response) => {
         setEntries(JSON.parse(response.data));
       });
@@ -21,7 +61,7 @@ function App() {
 
   return (
     <div className="flex items-center justify-center flex-col w-screen min-h-screen my-5">
-      <p>Proteous</p>
+      <Toaster />
       <div
         className="flex flex-col items-center mt-5 border border-black rounded-lg text-center
       w-[75%] min-h-fit min-h-fit"
@@ -30,7 +70,10 @@ function App() {
           <div className="w-full flex flex-col items-start space-y-4">
             <div className="w-full flex flex-col items-start">
               <p>Project</p>
-              <input className="border border-black rounded-lg text-center w-full" />
+              <input
+                className="border border-gray-300 px-2 w-full h-[43px]"
+                placeholder="Define the name of the project"
+              />
             </div>
             <div className="w-full flex flex-col items-start">
               <p>Upload your own file</p>
@@ -46,53 +89,50 @@ function App() {
                 }))}
                 onClick={async (id) => {
                   const predictions = await axios.post(
-                    "https://proteous-606b94fbd95c.herokuapp.com/get_best_variants",
-                    // "http://localhost:8000/get_best_variants",
+                    // "https://proteous-606b94fbd95c.herokuapp.com/get_best_variants",
+                    "http://localhost:8000/get_best_variants",
                     {
                       sequence: entries[id].protein_sequence,
                     }
                   );
-                  setPrediction(predictions.data.sequences);
+                  setPrediction(predictions.data);
                 }}
               />
             </div>
-            {prediction.length ? (
+            {prediction.sequences.length ? (
               <div className="w-full flex flex-col items-start">
                 <p>Best Variants</p>
                 <Table
-                  metric={false}
-                  entries={prediction?.map((entry, id) => ({
+                  metric={true}
+                  entries={prediction?.sequences.map((entry, id) => ({
                     sequence: entry.slice(0, 30),
                     id,
+                    metric: prediction.tms[id],
                   }))}
-                  onClick={async (id) => {
-                    const predictions = await axios.post(
-                      "https://proteous-606b94fbd95c.herokuapp.com/predict",
-                      // "http://localhost:8000/predict",
-                      {
-                        sequence: prediction[id],
-                      }
-                    );
-                    setTm(predictions.data.tm);
+                  onClick={(id) => {
+                    navigator.clipboard.writeText(prediction.sequences[id]);
+                    notify();
                   }}
                 />
-                {tm && (
-                  <div className="flex flex-col items-start">
-                    <p>Predicted TM</p>
-                    <div
-                      className="border border-black my-2 p-4 h-12 w-max
-               rounded-lg truncate flex "
-                    >
-                      {tm}
-                    </div>
-                  </div>
-                )}
               </div>
             ) : null}
           </div>
-          <div className="w-full flex flex-col items-start h-[15%]">
-            <p>Objective</p>
-            <input className="w-full border border-black rounded-lg text-center" />
+          <div className="w-full flex flex-col space-y-4">
+            <div className="w-full flex flex-col items-start h-[15%]">
+              <p>Objective</p>
+              <Dropdown
+                className="w-full border-gray-300 rounded-lg"
+                options={options}
+                placeholder="Select an option"
+              />
+            </div>
+            <div className="border border-gray-300 rounded-lg w-full h-full">
+              <img
+                src={proteinImages[getRandomInt(11)]}
+                alt="3d protein"
+                className="w-full"
+              ></img>
+            </div>
           </div>
         </div>
       </div>
--- src/components/Table.tsx
+++ src/components/Table.tsx
@@ -13,24 +13,20 @@ const Table = ({
   onClick?: (id: number) => void;
 }) => {
   return (
-    <div className="border rounded-lg border-black p-2 text-gray-700">
+    <div className="w-full border  border-gray-300 p-2 text-gray-700">
       <table className="table-auto">
         <thead>
           <tr>
             <th>Protein Sequence</th>
-            {metric && <th>Metric</th>}
+            {metric ? <th>Metric</th> : null}
           </tr>
         </thead>
-        <tbody className="text-center">
-          <td className="text-center">
+        <tbody className="text-center w-full">
+          <td className="text-center w-full">
             {entries.map((entry) => {
               return (
                 <tr
-                  className={`text-center w-full px-2 ${
-                    metric
-                      ? ""
-                      : "hover:bg-gray-100 hover:text-gray-900 cursor-pointer rounded-lg"
-                  }`}
+                  className={`text-center w-full px-2 hover:bg-gray-100 hover:text-gray-900 cursor-pointer rounded-lg`}
                   onClick={(id) => onClick(entry.id)}
                 >
                   {entry.sequence}
@@ -38,13 +34,17 @@ const Table = ({
               );
             })}
           </td>
-          {metric && (
-            <td className="flex flex-col items-center text-center w-full">
+          {metric ? (
+            <td className="flex flex-col items-center text-center w-full border border-black">
               {entries.map((entry) => {
-                return <tr className="text-center w-full">{entry.metric}</tr>;
+                return (
+                  <tr className="text-center w-full">
+                    {entry.metric.toString().slice(0, 12)}
+                  </tr>
+                );
               })}
             </td>
-          )}
+          ) : null}
         </tbody>
       </table>
     </div>
