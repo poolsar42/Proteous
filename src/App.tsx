a3ad1e0dad1f64ad9a8cfbda81afdaa76db4a7c5
emil <33105890+poolsar42@users.noreply.github.com>
2023-10-17 11:44:09 +0200

    adding upload button

--- src/App.tsx
+++ src/App.tsx
@@ -13,39 +13,41 @@ function App() {
   useEffect(() => {
     axios
       .get("https://proteous-606b94fbd95c.herokuapp.com/all-entries")
+      // .get("http://localhost:8000/all-entries")
       .then((response) => {
-        //http://localhost:8000/all-entries
         setEntries(JSON.parse(response.data));
       });
   }, []);
 
   return (
-    <div className="flex items-center justify-center flex-col w-screen h-screen">
+    <div className="flex items-center justify-center flex-col w-screen min-h-screen my-5">
       <p>Proteous</p>
       <div
         className="flex flex-col items-center mt-5 border border-black rounded-lg text-center
-      w-[75%] h-[85%] min-h-fit"
+      w-[75%] min-h-fit min-h-fit"
       >
-        <div className="flex flex-row justify-between w-full px-5">
-          <div className="flex flex-col items-start space-y-4">
-            <div>
-              <div className="flex flex-col items-start">
-                <p>Project</p>
-                <input className="border border-black rounded-lg text-center" />
-              </div>
+        <div className="flex flex-row justify-between w-full px-5 space-x-4 py-4">
+          <div className="w-full flex flex-col items-start space-y-4">
+            <div className="w-full flex flex-col items-start">
+              <p>Project</p>
+              <input className="border border-black rounded-lg text-center w-full" />
             </div>
-            <div className="flex flex-col items-start">
+            <div className="w-full flex flex-col items-start">
+              <p>Upload your own file</p>
+              <input type="file" />
+            </div>
+            <div className="w-full flex flex-col items-start">
               <p>Database Entries</p>
               <Table
                 metric={false}
                 entries={entries?.map((entry, id) => ({
-                  sequence: entry.protein_sequence.slice(0, 18),
+                  sequence: entry.protein_sequence.slice(0, 30),
                   id,
                 }))}
                 onClick={async (id) => {
                   const predictions = await axios.post(
                     "https://proteous-606b94fbd95c.herokuapp.com/get_best_variants",
-                    //"http://localhost:8000/get_best_variants",
+                    // "http://localhost:8000/get_best_variants",
                     {
                       sequence: entries[id].protein_sequence,
                     }
@@ -55,18 +57,18 @@ function App() {
               />
             </div>
             {prediction.length ? (
-              <div className="flex flex-col items-start">
+              <div className="w-full flex flex-col items-start">
                 <p>Best Variants</p>
                 <Table
                   metric={false}
                   entries={prediction?.map((entry, id) => ({
-                    sequence: entry.slice(0, 18),
+                    sequence: entry.slice(0, 30),
                     id,
                   }))}
                   onClick={async (id) => {
                     const predictions = await axios.post(
                       "https://proteous-606b94fbd95c.herokuapp.com/predict",
-                      //"http://localhost:8000/predict"
+                      // "http://localhost:8000/predict",
                       {
                         sequence: prediction[id],
                       }
@@ -88,11 +90,9 @@ function App() {
               </div>
             ) : null}
           </div>
-          <div>
-            <div className="flex flex-col items-start h-[15%]">
-              <p>Objective</p>
-              <input className="border border-black rounded-lg text-center" />
-            </div>
+          <div className="w-full flex flex-col items-start h-[15%]">
+            <p>Objective</p>
+            <input className="w-full border border-black rounded-lg text-center" />
           </div>
         </div>
       </div>
